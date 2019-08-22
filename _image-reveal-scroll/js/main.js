b8ab62d137b2afc1658872a878a09d0d7be51f4d
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-08-22 18:58:04 -0500

    animate objects

---
+++ _image-reveal-scroll/js/main.js
@@ -0,0 +1,8 @@
+var animateIn = new TimelineMax();
+
+animateIn
+.fromTo('.overlay', 2, {skewX:30, scale: 1.5}, {skewX: 0, xPercent: 100, transformOrigin: '0% 100%', ease: Power4.easeOut})
+.from('.project-info', 1, {scaleY: 0, transformOrigin: "bottom left"}, '-=1.5')
+.from('.small-title', 0.2, {autoAlpha: 0, y:30, ease: Power4.easeOut}, '-=0.8')
+.from('.project-link', 0.3, {autoAlpha: 0, y:30, ease: Power4.easeOut}, '-=0.8')
+.from('h4', 0.3, {autoAlpha: 0, y:30, ease: Power4.easeOut}, '-=0.8')
