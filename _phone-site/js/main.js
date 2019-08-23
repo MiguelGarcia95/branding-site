59ff8af202276ec1852122db8691d9a078940891
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-08-23 16:04:57 -0500

    animate  phone

--- _phone-site/js/main.js
+++ _phone-site/js/main.js
@@ -2,5 +2,18 @@ var tlFirstScroll = new TimelineMax();
 
 tlFirstScroll
 .set('.iphone-image-wrapper', {scale: 4, transformOrigin: 'center top'})
-.to('.iphone-image-wrapper', 3, {scale: 2.5, y: '-40%'})
-.to('.iphone-image-wrapper', 3, {scale: 1, y: '0%'})
+.to('.iphone-image-wrapper', 3, {scale: 2.5, y: '-50%'})
+.to('.iphone-image-wrapper', 3, {scale: 1, y: '0%'});
+
+var controller = new ScrollMagic.Controller();
+
+// First Scene
+
+var firstScene = new ScrollMagic.Scene({
+  triggerElement: '.trigger1',
+  triggerHook: 0,
+  duration: '100%',
+})
+.setTween(tlFirstScroll)
+.addIndicators()
+.addTo(controller)
