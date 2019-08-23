f8e18e4bb33d5319cc1af6f04f77b84a8d044104
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-08-22 19:26:29 -0500

    animate all loop items

--- _image-reveal-scroll/js/main.js
+++ _image-reveal-scroll/js/main.js
@@ -1,17 +1,26 @@
 var controller = new ScrollMagic.Controller();
-var animateIn = new TimelineMax();
 
-animateIn
-.fromTo('.overlay', 2, {skewX:30, scale: 1.5}, {skewX: 0, xPercent: 100, transformOrigin: '0% 100%', ease: Power4.easeOut})
-.from('.project-info', 1, {scaleY: 0, transformOrigin: "bottom left"}, '-=1.5')
-.from('.small-title', 0.2, {autoAlpha: 0, y:30, ease: Power4.easeOut}, '-=0.8')
-.from('.project-link', 0.3, {autoAlpha: 0, y:30, ease: Power4.easeOut}, '-=0.8')
-.from('h4', 0.3, {autoAlpha: 0, y:30, ease: Power4.easeOut}, '-=0.8');
+$('.project').each(function() {
 
-// Scrollmagic scene
+  var picOverlay = $(this).find('.overlay');
+  var projectInfo = $(this).find('.project-info');
+  var smallTitle = $(this).find('.small-title');
+  var projectLink = $(this).find('.project-link');
+  var animateIn = new TimelineMax();
 
-var scene = new ScrollMagic.Scene({
-  triggerElement: '.project'
-})
-.setTween(animateIn)
-.addTo(controller)
+  animateIn
+  .fromTo('.overlay', 2, {skewX:30, scale: 1.5}, {skewX: 0, xPercent: 100, transformOrigin: '0% 100%', ease: Power4.easeOut})
+  .from('.project-info', 1, {scaleY: 0, transformOrigin: "bottom left"}, '-=1.5')
+  .from('.small-title', 0.2, {autoAlpha: 0, y:30, ease: Power4.easeOut}, '-=0.8')
+  .from('.project-link', 0.3, {autoAlpha: 0, y:30, ease: Power4.easeOut}, '-=0.8')
+  .from('h4', 0.3, {autoAlpha: 0, y:30, ease: Power4.easeOut}, '-=0.8');
+
+  // Scrollmagic scene
+
+  var scene = new ScrollMagic.Scene({
+    triggerElement: this
+  })
+  .addIndicators()
+  .setTween(animateIn).addTo(controller);
+
+});
